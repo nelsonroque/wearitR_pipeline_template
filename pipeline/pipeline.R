1f0eed15aec162d9a79d1cd492edc3693c0df246
Nelson Roque <nelsonroquejr@gmail.com>
2022-03-02 10:55:24 -0500

    add install to pipeline script

--- pipeline/pipeline.R
+++ pipeline/pipeline.R
@@ -1,3 +1,19 @@
+# change to FALSE after initial package install -----
+FIRST_RUN = TRUE
+
+# install packages on first run ----
+if(FIRST_RUN) {
+    # list of packages required on machine if developing on this R Package ---
+    packages_req_dev = c("rlist", "skimr", "tidyverse", "devtools") # optional: "dataReporter"
+
+    # install them ---
+    install.packages(packages_req_dev)
+}
+
+# install latest package version ----
+devtools::install_github("nelsonroque/wearitR", force=T)
+
+# load lib ----
 library(wearitR)
 
 # `data_path` = directory relative to the RProj (in this case, `data`)

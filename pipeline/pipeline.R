90e6e863869f987d4d224150a009fc61a33429b0
Nelson Roque <nelsonroquejr@gmail.com>
2022-03-15 15:01:51 -0400

    up

--- pipeline/pipeline.R
+++ pipeline/pipeline.R
@@ -21,5 +21,5 @@ library(wearitR)
 config_file = "config.json"
 data_path = "data"
 study_config = read_studyconfig(config_file)
-run_pipeline_survey(data_path, config_file, use_labels = T) # old data
-run_pipeline_cogdata(data_path, config_file) # old data
+run_pipeline_survey(data_path, config_file, use_labels = T)
+run_pipeline_cogdata(data_path, config_file)
